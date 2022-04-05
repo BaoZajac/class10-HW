b5950b26e3bd5a2db13cb90a80c3adea83c2c798
Basia <bao.zajac@gmail.com>
2022-04-05 22:24:45 +0200

    przerobienie kodu na metody klasy __init__, __getitem__, items,  odp_o_deszczu, spr_danych_o_deszczu, wczytaj_dane_o_pogodzie, przerabianie if len w trakcie

--- weather.py
+++ weather.py
@@ -3,61 +3,78 @@ import datetime
 import requests
 import json
 
+URL = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
+PARAMS = {"q": "san francisco,us", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
+
 
 class WeatherForecast:
-    def __init__(self):
-        ...
+    def __init__(self, apikey):
+        self.apikey = apikey
+        self.wczytaj_dane_o_pogodzie()
 
     def __getitem__(self, date):
-        return self.spr_danych_o_deszczu()[date]
+        return self.spr_danych_o_deszczu(date)
 
     def __iter__(self):
         ...
 
     def items(self):
-        ...
+        return self.saved_history.items()
 
-    def odp_o_deszczu(self):
-        print(f"Opady w dniu {date_to_check}: {saved_history[date_to_check]}")
-        if saved_history[date_to_check] > 0:
+    def odp_o_deszczu(self, date_to_check2):
+        print(f"Opady w dniu {date_to_check2}: {self.saved_history[date_to_check2]}")
+        if self.saved_history[date_to_check2] > 0:
             return "Będzie padać"
         else:
             return "Nie będzie padać"
 
-    def spr_danych_o_deszczu(self):
-        if date_to_check in saved_history:
-            self.odp_o_deszczu()
+    def spr_danych_o_deszczu(self, date_to_check):
+        if date_to_check in self.saved_history:
+            return self.odp_o_deszczu(date_to_check)
         else:
-            from datetime import datetime  # TODO: ? -> dlaczego nie zaciąga fromtimestamp z import datetime tylko trzeba wpisać dodatkowy import?
-            url = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
-            params = {"q": "san francisco,us", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
-            headers = {"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com", "X-RapidAPI-Key": sys.argv[1]}
-            response = requests.get(url, headers=headers, params=params)
+            from datetime import datetime
+            headers = {"X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com", "X-RapidAPI-Key": self.apikey}
+            response = requests.get(URL, headers=headers, params=PARAMS)
             user_object = response.json()
             for element in user_object["list"]:
                 rain = element.get("pop", 0)
                 date_info = element["dt"]
                 day = datetime.fromtimestamp(date_info).date()
-                saved_history[str(day)] = rain
-            if date_to_check in saved_history:
-                self.odp_o_deszczu()
-                with open("history_of_weather.json", "w") as f2:  #TODO: nadpisuje wszystko, zmienić na możliwość dopisywania?
-                    json.dump(saved_history, f2)
+                self.saved_history[str(day)] = rain
+            if date_to_check in self.saved_history:
+                with open("history_of_weather.json", "w") as f2:
+                    json.dump(self.saved_history, f2)
+                return self.odp_o_deszczu(date_to_check)
             else:
                 print("Nie wiem - zapytanie o datę spoza bazy")
 
+    def wczytaj_dane_o_pogodzie(self):
+        with open("history_of_weather.json", "r") as f:
+            self.saved_history = json.load(f)
 
-with open("history_of_weather.json", "r") as f:
-    saved_history = json.load(f)
 
-wf = WeatherForecast()
-date_to_check = sys.argv[2]
-wf[date_to_check]
+wf = WeatherForecast(sys.argv[1])
+# wf.spr_danych_o_deszczu(sys.argv[2])
+print(wf[sys.argv[2]])
 
-#
+
+# date_to_check = sys.argv[2]
+# wf[date_to_check]
+
+
+if len(sys.argv) == 2 or len(sys.argv) == 3:
+    wf = WeatherForecast(sys.argv[1])
+    if len(sys.argv) == 2:
+        date_to_check = str(datetime.datetime.now().date() + datetime.timedelta(days=1))
+    elif len(sys.argv) == 3:
+        date_to_check = sys.argv[2]
+    print(wf[date_to_check])
+else:
+    print("Błąd - wprowadzono niepoprawne dane wejściowe z std")
+
 # if len(sys.argv) == 3:  # podano datę na wejściu
 #     API_key = sys.argv[1]
-#     date_to_check = sys.argv[2]    # TODO: sprawdzać popoprawność podania daty?
+#     date_to_check = sys.argv[2]
 #     wf[date_to_check]    # spr_danych_o_deszczu()
 # elif len(sys.argv) == 2:  # nie podano daty na wejściu
 #     API_key = sys.argv[1]
