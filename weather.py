461a5b276905e83d3d4a11f01556f42f93196ba4
Basia <bao.zajac@gmail.com>
2022-04-08 15:37:53 +0200

    poprzedni commit dotyczy tego pliku

--- weather.py
+++ weather.py
@@ -2,10 +2,10 @@ import sys
 import datetime
 import requests
 import json
+import os
 
 URL = "https://community-open-weather-map.p.rapidapi.com/forecast/daily"
 PARAMS = {"q": "san francisco,us", "lat": "35", "lon": "139", "cnt": "10", "units": "metric or imperial"}
-wf = None
 
 
 class WeatherForecast:
@@ -16,20 +16,8 @@ class WeatherForecast:
     def __getitem__(self, date):
         return self.spr_danych_o_deszczu(date)
 
-    # def __iter__(self):
-    #     a = self.saved_history.keys()
-    #     return a
-
-    # def __next__(self):
-    #     a = self.saved_history.keys()
-    #     return a
-    #     if element in self.user_object["list"]:
-    #
-    # for element in user_object["list"]:
-    #     rain = element.get("pop", 0)
-    #     date_info = element["dt"]
-    #     day = datetime.datetime.fromtimestamp(date_info).date()
-    #     self.saved_history[str(day)] = rain
+    def __iter__(self):
+        return iter(self.saved_history.keys())
 
     def wczytaj_dane_o_pogodzie(self):
         with open("history_of_weather.json", "r") as f:
@@ -40,7 +28,6 @@ class WeatherForecast:
 
     def odp_o_deszczu(self, date_to_check2):
         # print(f"Opady w dniu {date_to_check2}: {self.saved_history[date_to_check2]}")
-        # print(self.saved_history.keys())
         if self.saved_history[date_to_check2] > 0:
             return f"{date_to_check2} -> Będzie padać"
         else:
@@ -66,22 +53,32 @@ class WeatherForecast:
                 return f"{date_to_check1} -> Nie wiem - zapytanie o datę spoza bazy"
 
 
+def separator():
+    w, h = os.get_terminal_size()
+    print(w * "-")
+
+
+wf = WeatherForecast(sys.argv[1])
+
 if len(sys.argv) == 2 or len(sys.argv) == 3:
-    wf = WeatherForecast(sys.argv[1])
     if len(sys.argv) == 2:
         date_to_check = str(datetime.datetime.now().date() + datetime.timedelta(days=1))
-    # elif len(sys.argv) == 3:
     else:
         date_to_check = sys.argv[2]
-    # if wf.spr_danych_o_deszczu(date_to_check) == "Nie wiem - zapytanie o datę spoza bazy":
-    #     print("Nie wiem - zapytanie o datę spoza bazy")
-    # else:
-    print(wf[date_to_check])
-else:
-    print("Błąd - wprowadzono niepoprawne dane wejściowe z std")
 
-print("pogoda jest znana dla następujących terminów: ")
-print(wf)                       #TODO: rozwiązać to
+separator()
+
+print("Informacja o opadach dla następującego terminu:   ", wf[date_to_check])
+
+separator()
+
+print("Pogoda znana jest dla następujących terminów:")
+lista_dat_z_pogoda = []
+for elem in wf:
+    lista_dat_z_pogoda.append(elem)
+print(lista_dat_z_pogoda)
+
+separator()
 
 if wf:
     print("Znane informacje o deszczu to:")
@@ -89,9 +86,3 @@ if wf:
     for idx in wf.items():
         lista_deszczu.append(idx)
     print(lista_deszczu)
-
-
-# slownik = {"a": 1, "b": 2}
-# print(slownik)
-# print(slownik.keys())
-# # print(slownik[0])
